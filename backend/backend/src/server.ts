060e2562f7b96f026fd5bdaf10db79f9ddb52d0b
paulsam20 <paulinesamyuktha.26it@licet.ac.in>
2026-02-18 16:39:41 +0530

    curd logics implemented and fixing nested repo structure

--- backend/backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-import connectDB from './config/db';
-import dotenv from 'dotenv';
-
-dotenv.config();
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
